c507e16d2e1528ca08e757cdd5114c96c85c2622
alper <replappdev@gmail.com>
2021-08-11 05:13:16 -0700

    search bar ui added

---
+++ GitHubFollowersApp/FavoritesListVC.swift
@@ -0,0 +1,21 @@
+//
+//  FavoritesListVC.swift
+//  GitHubFollowersApp
+//
+//  Created by calatinalper on 11.08.2021.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBlue
+    }
+    
+
+   
+
+}
---
+++ GitHubFollowersApp/GFTextField.swift
@@ -0,0 +1,41 @@
+//
+//  GFTextField.swift
+//  GitHubFollowersApp
+//
+//  Created by calatinalper on 11.08.2021.
+//
+
+import UIKit
+
+class GFTextField: UITextField {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    //3 line code above otomatically fill bc storyboard initializers
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.systemGray4.cgColor
+        
+        textColor = .label
+        tintColor = .label
+        textAlignment = .center
+        font = UIFont.preferredFont(forTextStyle: .title2)
+        adjustsFontSizeToFitWidth = true
+        minimumFontSize = 12
+        
+        backgroundColor = .tertiarySystemBackground
+        autocorrectionType = .no
+        
+        placeholder = "Enter a username"
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+}
